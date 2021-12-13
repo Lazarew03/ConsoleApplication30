8ce613e1b94b41ce718b83fe45f24bf2e623747a
user <user@C1011>
2021-12-13 14:12:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int t = true, p = 101010;
+
+    if (t && (p % 3==0))
+    {
+
+        bool a = true, b = false;
+        bool res = (t && (p % 3 == 0));
+        cout << "Result: " << boolalpha << res;
+        return 0;
+
+
+    }
+
+
+
+    system("pause");
+    return 0;
+}
